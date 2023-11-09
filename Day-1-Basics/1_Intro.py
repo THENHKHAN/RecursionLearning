0ea28d567d8e4c222ed6a2cf8a2013977fe4e8c3
Noorul Huda Khan <92791314+THENHKHAN@users.noreply.github.com>
2023-11-09 16:36:47 +0530

    Day-1 basic intro and file strcuturing uploaded

---
+++ Day-1-Basics/1_Intro.py
@@ -0,0 +1,24 @@
+
+def print5(n) :
+    print(n)
+
+def print4(n) :
+    print(n)
+    print5(5)
+
+def print3(n) :
+    print(n)
+    print4(4)
+def print2(n):
+    print(n)
+    print3(3)
+
+def print1(n) :
+    print(n)
+    print2(2)
+
+def printCall() :
+    print1(1)
+
+
+printCall()
